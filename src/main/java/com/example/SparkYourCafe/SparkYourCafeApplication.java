50a7bccfea57f8734fb876110c4d4cbec8816fe1
91876 <91876@LAPTOP-5SBTI9GN>
2024-05-24 17:34:41 +0530

    initial commit

---
+++ src/main/java/com/example/SparkYourCafe/SparkYourCafeApplication.java
@@ -0,0 +1,13 @@
+package com.example.SparkYourCafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkYourCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkYourCafeApplication.class, args);
+	}
+
+}
